faaf096e5abb4ce1c2abfe8806be559b98ce7e4a
torichaan1225 <torichaan1225@gmail.com>
2026-03-15 13:30:39 +0900

    トップページログイン変移変更

--- src/components/Pages/Top/index.tsx
+++ src/components/Pages/Top/index.tsx
@@ -3,14 +3,24 @@ import styles from "./index.module.css";
 const Top = () => {
   return (
     <div>
-      <h1>TopPage</h1>
-      <div className={styles["top-links"]}>
-        <Link to="/show-all-furniture">Go to Show All Furniture{"\n"}</Link>
-        <Link to="/assemble-furniture">Go to Assemble Furniture{"\n"}</Link>
-        <Link to="/make-furniture">Go to Make Furniture{"\n"}</Link>
+      <header className={styles.header}>
+        <h1 className={styles.title}>素晴らしいタイトル</h1>
+        <div className={styles["to-login"]}>
+          <Link to="/login" className={styles["bottun"]}>
+            　ログイン　
+            <br />
+            サインアップ
+          </Link>
+        </div>
+      </header>
+      <div className={styles["top-page"]}>
+        <h1>TopPage</h1>
+        <div className={styles["top-links"]}>
+          <Link to="/show-all-furniture">Go to Show All Furniture{"\n"}</Link>
+          <Link to="/assemble-furniture">Go to Assemble Furniture{"\n"}</Link>
+          <Link to="/make-furniture">Go to Make Furniture{"\n"}</Link>
+        </div>
       </div>
-      <h1>Login/Signup</h1>
-      <Link to="/login">Go to Login</Link>
     </div>
   );
 };
